bfc5ad9bcdaff3e91ce0981f8319d67f42a687d0
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-02 20:58:12 -0700

    extra code removed
    extra code removed to make UI look good

--- script.js
+++ script.js
@@ -1,67 +1,58 @@
 let userQuestion = document.querySelector("#userAsk");
-// let userChoice = document.querySelector("#userResponse");
 let btnPlay = document.querySelector("#btn-play");
-// let dispQuestion = document.querySelector("#dispAsk");
 let dispAnswer = document.querySelector("#dispAns");
 let btnReset = document.querySelector("#btn-reset");
-
 let hideSeek = document.querySelector(".user-reply");
 
-hideSeek.style.visibility="hidden";
-
-
+hideSeek.style.visibility = "hidden";
 
 
 // Function for the main logic of the game
 function findAnswer(choiceResponse) {
 
-    if (choiceResponse <= 50 ) {
-        return("It is certain");
-        }  else if (choiceResponse <=100) {
-        return('It is decidedly so')
-        } else if (choiceResponse <=150) {
-            return("Without a doubt")
-        } else if (choiceResponse <=200) {
-            return("Yes - definitely")
-        } else if (choiceResponse <=250) {
-            return('You may rely on it')
-        } else if (choiceResponse <=300) {
-            return("As I see it, yes")
-        } else if (choiceResponse <=350) {
-            return(" Most Likely")
-        } 
-
-      else if (choiceResponse <=400) {
-        return('Outlook good')
-      } else if (choiceResponse <=450) {
-         return("Yes")
-      } else if (choiceResponse <=500) {
-         return("Signs point to yes")
-      } else if (choiceResponse <=550) {
-        return('Reply hazy, try again')
-      } else if (choiceResponse <=600) {
-         return("Ask again later")
-      } else if (choiceResponse <=650) {
-         return(" Better not to tell you now")
-      } 
-     
-      else if (choiceResponse <=700) {
-        return('Cannot predict now')
-      } else if (choiceResponse <=750) {
-         return("Concentrate and ask again")
-      } else if (choiceResponse <=800) {
-         return("Don't count on it")
-      } else if (choiceResponse <=850) {
-        return('My reply is no')
-      } else if (choiceResponse <=900) {
-         return("My sources say no")
-      } else if (choiceResponse <=950) {
-         return("Outlook not so good")
-      } else if (choiceResponse <=1000) {
-         return("Very doubtful")
-    }  else {
-           return('Ask another question, keep playing');
-       } 
+   if (choiceResponse <= 50) {
+      return ("It is certain");
+   } else if (choiceResponse <= 100) {
+      return ('It is decidedly so')
+   } else if (choiceResponse <= 150) {
+      return ("Without a doubt")
+   } else if (choiceResponse <= 200) {
+      return ("Yes - definitely")
+   } else if (choiceResponse <= 250) {
+      return ('You may rely on it')
+   } else if (choiceResponse <= 300) {
+      return ("As I see it, yes")
+   } else if (choiceResponse <= 350) {
+      return (" Most Likely")
+   } else if (choiceResponse <= 400) {
+      return ('Outlook good')
+   } else if (choiceResponse <= 450) {
+      return ("Yes")
+   } else if (choiceResponse <= 500) {
+      return ("Signs point to yes")
+   } else if (choiceResponse <= 550) {
+      return ('Reply hazy, try again')
+   } else if (choiceResponse <= 600) {
+      return ("Ask again later")
+   } else if (choiceResponse <= 650) {
+      return (" Better not to tell you now")
+   } else if (choiceResponse <= 700) {
+      return ('Cannot predict now')
+   } else if (choiceResponse <= 750) {
+      return ("Concentrate and ask again")
+   } else if (choiceResponse <= 800) {
+      return ("Don't count on it")
+   } else if (choiceResponse <= 850) {
+      return ('My reply is no')
+   } else if (choiceResponse <= 900) {
+      return ("My sources say no")
+   } else if (choiceResponse <= 950) {
+      return ("Outlook not so good")
+   } else if (choiceResponse <= 1000) {
+      return ("Very doubtful")
+   } else {
+      return ('Ask another question, keep playing');
+   }
 }
 
 
@@ -69,31 +60,24 @@ function findAnswer(choiceResponse) {
 function playClick() {
 
    if (userQuestion.value.trim() == "") {
-       alert("Please write a question")
+      alert("Please write a question")
+   } else {
+      hideSeek.style.visibility = "visible";
+      let userChoice = (Math.random()) * 1000;
+      let showAnswer = findAnswer(userChoice);
+      dispAnswer.innerText = showAnswer;
    }
-   else{
-      hideSeek.style.visibility="visible"; 
-   // dispQuestion.innerText = userQuestion.value;
-   let userChoice = (Math.random()) * 1000;
-   let showAnswer = findAnswer(userChoice); 
-   dispAnswer.innerText = showAnswer; 
-   // userChoice = ; 
-   }
-   
 }
 
 btnPlay.addEventListener("click", playClick);
 
-
 // Reset Button
 function resetGame() {
-    console.log("Reset button clicked");  
-    hideSeek.style.visibility="hidden";
-    
-    userQuestion.value = null ;
-   //  dispQuestion.innerText = null; 
-    dispAnswer.innerText = null ;
+   console.log("Reset button clicked");
+   hideSeek.style.visibility = "hidden";
 
+   userQuestion.value = null;
+   dispAnswer.innerText = null;
 }
 
 btnReset.addEventListener("click", resetGame);
